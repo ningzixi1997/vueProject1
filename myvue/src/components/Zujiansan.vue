a74ca759c175bbe9e69b489e15ddf7b9afe42ea8
DESKTOP-5TBM399\lx <1379444940@qq.com>
2019-05-06 19:45:06 +0800

    1231

---
+++ myvue/src/components/Zujiansan.vue
@@ -0,0 +1,16 @@
+<template>
+    <fieldset>
+      <legend><h2>这是组件3</h2></legend>
+      <div>这是组件三</div>
+    </fieldset>
+</template>
+
+<script>
+    export default {
+        name: "Zujiansan"
+    }
+</script>
+
+<style scoped>
+
+</style>
