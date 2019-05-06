c2e91031c5974ef94d5eeb6913412eb77668de59
DESKTOP-5TBM399\lx <2572992933@qq.com>
2019-05-06 19:27:57 +0800

    项目卡萨丁

---
+++ myvue/src/components/Ncom.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="ncom">
+      <h2>这是组件1</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Ncom"
+    }
+</script>
+
+<style scoped>
+  .ncom h2{
+    color: blue;
+  }
+</style>
--- myvue/src/router/index.js
+++ myvue/src/router/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Ncom from '../components/Ncom'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {path:'/zj1'},
+    {path:'/zj1',component:Ncom},
     {path:'/zj2'},
     {path:'/zj3'},
   ]
